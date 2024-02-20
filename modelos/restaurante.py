169d349d0f91d9b5bf32dbbc818a03ad19600c9e
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2024-02-20 13:47:51 -0300

    Aula 2.2

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,22 +1,11 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Bistrô'
-restaurante_praca.categoria = 'Italiana'
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Pizza Place'
-restaurante_pizza.categoria = 'Fast Food'
-restaurante_pizza.ativo = True
-
-print('É Fast Food' if restaurante_pizza.categoria == 'Fast Food' else 'Não é Fast Food')
-print(f'{restaurante_pizza.nome} - {restaurante_pizza.categoria}')
-
-print(dir(restaurante_praca))
 print(vars(restaurante_praca))
-print('Restaurante ativo' if restaurante_praca.ativo else 'Restaurante inativo')
-
-categoria = Restaurante.categoria
+print(vars(restaurante_pizza))
