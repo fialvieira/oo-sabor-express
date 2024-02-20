d60779f82ca34f0e3fda874bd7d58941378bdec8
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2024-02-20 14:11:44 -0300

    Aula 2.4

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,11 +1,20 @@
 class Restaurante:
-    def __init__(self, nome, categoria):
+    restaurantes = []
+    def __init__(self, nome, categoria) -> None:
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self) -> str:
+        return f'nome: {self.nome} | categoria: {self.categoria} | ativo: {self.ativo}'
+    
+    def listar_restaurantes():
+        print(f"{'nome'.ljust(20)} | {'categoria'.ljust(20)} | ativo")
+        for restaurante in Restaurante.restaurantes:
+            print(f'{str(restaurante.nome).ljust(20)} | {str(restaurante.categoria).ljust(20)} | {restaurante.ativo}')
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+Restaurante.listar_restaurantes()
