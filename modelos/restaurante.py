db3c27e1a17727f455886c94dfd88c8494673631
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2024-02-20 14:38:48 -0300

    Lição 3.4

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,20 +1,29 @@
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria) -> None:
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self) -> str:
-        return f'nome: {self.nome} | categoria: {self.categoria} | ativo: {self.ativo}'
+        return f'nome: {self._nome} | categoria: {self._categoria} | ativo: {self.ativo}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f"{'nome'.ljust(20)} | {'categoria'.ljust(20)} | ativo")
-        for restaurante in Restaurante.restaurantes:
-            print(f'{str(restaurante.nome).ljust(20)} | {str(restaurante.categoria).ljust(20)} | {restaurante.ativo}')
+        for restaurante in cls.restaurantes:
+            print(f'{str(restaurante._nome).ljust(20)} | {str(restaurante._categoria).ljust(20)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return 'verdadeiro' if self._ativo else 'falso'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
